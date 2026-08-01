bc564e57ffd2319aa37f96a1fa65551b0b649c3b
Nikunj Raval <rockeydon201@gmail.com>
2026-08-01 14:54:37 +0530

    update socket

--- src/api/axois.js
+++ src/api/axois.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: "https://resturant-demo.onrender.com/api/",
+//   timeout: 10000,
+// });
+
 const api = axios.create({
-    // baseURL: "http://localhost:5000/api",
-  //   baseURL: import.meta.env.VITE_BASE_URL,
-  baseURL: "https://resturant-demo.onrender.com/api/",
+   baseURL: "https://south-delights.onrender.com/api",
   timeout: 10000,
 });
-
 api.interceptors.request.use((config) => {
   console.log(
     `🚀 API Calling => ${config.method?.toUpperCase()} ${config.baseURL}${config.url}`,
--- src/socket.js
+++ src/socket.js
@@ -1,13 +1,18 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://resturant-demo.onrender.com/api", {
+// const socket = io("https://resturant-demo.onrender.com/api", {
+//   autoConnect: true,
+//   reconnection: true,          // default true છે, પણ explicit રાખો
+//   reconnectionAttempts: Infinity,
+//   reconnectionDelay: 1000,
+//   reconnectionDelayMax: 5000,
+//   timeout: 20000,               // connection timeout વધારો (cold start માટે)
+//   transports: ["websocket", "polling"],  // fallback રાખો
+// });
+
+const socket =  io("https://south-delights.onrender.com", {
   autoConnect: true,
-  reconnection: true,          // default true છે, પણ explicit રાખો
-  reconnectionAttempts: Infinity,
-  reconnectionDelay: 1000,
-  reconnectionDelayMax: 5000,
-  timeout: 20000,               // connection timeout વધારો (cold start માટે)
-  transports: ["websocket", "polling"],  // fallback રાખો
+   withCredentials: true
 });
 
 export default socket;
